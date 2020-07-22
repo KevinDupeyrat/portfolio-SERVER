9ffc6abe33ae7fe91051a62d1a0fea554f0f2a9e
Dupeyrat Kevin <kevin.dupeyrat@gmail.com>
2020-07-22 19:00:47 +0200

    Add Configuration for API Path Controler to 'v1'

--- portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/TestAPIResource.java
+++ portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/TestAPIResource.java
@@ -2,6 +2,7 @@ package com.dupeyrat.portfolio.web.rest.v1;
 
 import com.dupeyrat.portfolio.core.exception.ExceptionMessage;
 import com.dupeyrat.portfolio.core.exception.GenericException;
+import com.dupeyrat.portfolio.web.config.V1APIController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/test")
+@RequestMapping("test")
 @Slf4j
+@V1APIController
 public class TestAPIResource {
 
     @RequestMapping(method = RequestMethod.GET)
