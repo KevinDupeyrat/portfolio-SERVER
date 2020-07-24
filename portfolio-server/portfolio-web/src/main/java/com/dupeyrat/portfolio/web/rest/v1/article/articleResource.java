4e617b1c824bb8cbd4e3b8acbb868b802d211481
Dupeyrat Kevin <kevin.dupeyrat@gmail.com>
2020-07-24 16:34:24 +0200

    Add Rest API Class for article, experience and user

---
+++ portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/article/articleResource.java
@@ -0,0 +1,13 @@
+package com.dupeyrat.portfolio.web.rest.v1.article;
+
+import com.dupeyrat.portfolio.web.config.V1APIController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("article")
+@Slf4j
+@V1APIController
+public class articleResource {
+}
