2c5f072d2202d38045ed109df71c7ea795fb2044
Dupeyrat Kevin <kevin.dupeyrat@gmail.com>
2020-07-25 22:13:29 +0200

    Add SSL for local Profil

--- portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/TestAPIResource.java
+++ portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/TestAPIResource.java
@@ -2,20 +2,18 @@ package com.dupeyrat.portfolio.web.rest.v1;
 
 import com.dupeyrat.portfolio.core.exception.ExceptionMessage;
 import com.dupeyrat.portfolio.core.exception.GenericException;
-import com.dupeyrat.portfolio.web.config.V1APIController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
+@RequestMapping("/api/v1")
 @Slf4j
-@V1APIController
 public class TestAPIResource {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping(path = "test")
     public ResponseEntity testApi() {
 
         throw new GenericException(ExceptionMessage.INIT_VALUE);
--- portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/user/userResource.java
+++ portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/user/userResource.java
@@ -1,6 +1,5 @@
 package com.dupeyrat.portfolio.web.rest.v1.user;
 
-import com.dupeyrat.portfolio.web.config.V1APIController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 @Slf4j
-@V1APIController
 public class userResource {
 }
