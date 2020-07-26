1c71337e1e21b1a4adaf070014dce2ffbce48c04
Dupeyrat Kevin <kevin.dupeyrat@gmail.com>
2020-07-26 14:37:59 +0200

    Add config for context path API

--- portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/TestAPIResource.java
+++ portfolio-server/portfolio-web/src/main/java/com/dupeyrat/portfolio/web/rest/v1/TestAPIResource.java
@@ -5,18 +5,28 @@ import com.dupeyrat.portfolio.core.exception.GenericException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("test")
 @Slf4j
 public class TestAPIResource {
 
-    @GetMapping(path = "test")
-    public ResponseEntity testApi() {
+    @GetMapping()
+    public ResponseEntity<String> testGetApi() {
+        return ResponseEntity.ok().body("Tu as fait un GET");
+    }
 
+    @GetMapping(path = "error")
+    public ResponseEntity<String> testGetException() {
         throw new GenericException(ExceptionMessage.INIT_VALUE);
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> testPostApi() {
+        return ResponseEntity.ok().body("Tu as fait un POST");
     }
 
 }
